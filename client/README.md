1fc753792fc26af2c7526176afe39fda1a41a2a2
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2024-09-05 15:41:16 +0300

    feat: add controller functionality

--- client/README.md
+++ client/README.md
@@ -3,8 +3,9 @@
 - I want to create a card generator app
 - in App.tsx, we defined a useState hook and initialized it to our card data
 - If the card data is not present, return null
-- Then we've defined a handleSubmit function 
+- Then we've defined a handleSubmit function
 
 ## Server side
 
 - Create an express server
+- postman test: http://127.0.0.1:5000/openai/generateimages
--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import cardRoutes from './src/routes/CardRoutes';
+import express from "express";
+import cors from "cors";
+import openaiRoutes from "./routes/OpenaiRoutes.js";
 
 const app = express();
-const dotenv = require('dotenv').config();
-const port = process.env.PORT || 3001;
 
+const port = process.env.PORT || 5000;
 
-
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-app.use('/api', cardRoutes);
+app.use("/openai", openaiRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
